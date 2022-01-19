ba35bb5a8419d23aa2776115b30d8d14408cd736
TizianThieringer <tizmaen@gmail.com>
2022-01-19 18:21:13 +0100

    Dino

---
+++ modelvshuman/models/pytorch/dino/vit_b.py
@@ -0,0 +1,80 @@
+import torch
+import torch.distributed as dist
+from torch import nn
+
+#import eval_linear as lin
+
+#Setup linear layer
+def build_model():
+    model = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
+    
+    return model
+
+class LinearClassifier(nn.Module):
+    """Linear layer to train on top of frozen features"""
+    def __init__(self, dim, num_labels=1000):
+        print('dim: ', dim)
+        super(LinearClassifier, self).__init__()
+        self.num_labels = num_labels
+        self.linear = nn.Linear(dim, num_labels)
+        self.linear.weight.data.normal_(mean=0.0, std=0.01)
+        self.linear.bias.data.zero_()
+
+    def forward(self, x):
+        # flatten
+        print('shape: ', x.shape)
+        x = x.view(x.size(0), -1)
+
+        # linear layer
+        return self.linear(x)
+    
+
+
+def build_linear_classifier():
+    linear_classifier = LinearClassifier(1536, 1000)
+    linear_classifier = linear_classifier.cuda()
+    linear_classifier = nn.parallel.DistributedDataParallel(linear_classifier)
+    linear_classifier.eval()
+    state_dict = torch.hub.load_state_dict_from_url(url="https://dl.fbaipublicfiles.com/dino/dino_vitbase8_pretrain/dino_vitbase8_linearweights.pth")['state_dict']
+    linear_classifier.load_state_dict(state_dict, strict=True)
+    
+    return linear_classifier
+
+
+
+
+#Sequentialise
+@torch.no_grad()
+class MyModel(nn.Module):
+    
+    def __init__(self):
+        nn.Module.__init__(self)
+        self.facebook_model = build_model()
+        self.lin_layer = build_linear_classifier()
+            
+    # forward
+    def forward(self, x):
+        intermediate_output = self.facebook_model.get_intermediate_layers(x)
+        output = torch.cat([i_o[:, 0] for i_o in intermediate_output], dim=-1)
+        
+        output = torch.cat((output.unsqueeze(-1), torch.mean(intermediate_output[-1][:, 1:], dim=1).unsqueeze(-1)), dim=-1)
+        output = output.reshape(output.shape[0], -1)
+
+            
+        output = self.lin_layer(output)
+        return output
+        #loss = nn.CrossEntropyLoss()(output, target)
+
+
+
+#Build whole model
+dist.init_process_group('gloo', init_method='file:///tmp/somefile', rank=0, world_size=1)
+
+#model = build_model()
+#linear_classifier = build_linear_classifier()
+model = MyModel()
+
+data = torch.ones((1, 3, 224, 224))
+print(model(data))
+
+
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import torch
 import torch.distributed as dist
+from torch import nn
+
 
 class LinearClassifier(torch.nn.Module):
     def __init__(self, dim, num_labels=1000):
@@ -12,33 +14,47 @@ class LinearClassifier(torch.nn.Module):
     def forward(self, x):
         # flatten
         x = x.view(x.size(0), -1)
+        intermediate_output = model.get_intermediate_layers(x, 1)
+        x = torch.cat([a[:, 0] for a in intermediate_output], dim=-1)
+        
+        x = torch.cat((x.unsqueeze(-1), torch.mean(intermediate_output[-1][:, 1:], dim=1).unsqueeze(-1)), dim=-1)
+        x = x.reshape(x.shape[0], -1)
 
         # linear layer
         return self.linear(x)
+    
+def get_intermediate_layers(self, x, n=1):
+        x = self.prepare_tokens(x)
+        # we return the output tokens from the `n` last blocks
+        output = []
+        for i, blk in enumerate(self.blocks):
+            x = blk(x)
+            if len(self.blocks) - i <= n:
+                output.append(self.norm(x))
+        return output
 
 
 dist.init_process_group('gloo', init_method='file:///tmp/somefile', rank=0, world_size=1)
 # load backbone
-model = torch.hub.load('facebookresearch/dino:main', 'dino_resnet50')
+model = torch.hub.load('facebookresearch/dino:main', 'dino_vitb8')
 
 #Setup linear layer
-linear_classifier = LinearClassifier(2048, 1000)
+linear_classifier = LinearClassifier(1536, 1000)
 linear_classifier = linear_classifier.cuda()
-linear_classifier = torch.nn.parallel.DistributedDataParallel(linear_classifier)
-state_dict = torch.hub.load_state_dict_from_url(url="https://dl.fbaipublicfiles.com/dino/dino_resnet50_pretrain/dino_resnet50_linearweights.pth")['state_dict']
+linear_classifier = nn.parallel.DistributedDataParallel(linear_classifier)
+linear_classifier.eval()
+state_dict = torch.hub.load_state_dict_from_url(url="https://dl.fbaipublicfiles.com/dino/dino_vitbase8_pretrain/dino_vitbase8_linearweights.pth")['state_dict']
 linear_classifier.load_state_dict(state_dict, strict=True)
+
+
 
 #Sequentialise
 model = torch.nn.Sequential(model,
                             linear_classifier)
 
-#inp = torch.ones((1, 3, 224, 224))
-#intermediate_output = model.get_intermediate_layers(inp, 1)
-#output = torch.cat([x[:, 0] for x in intermediate_output], dim=-1)
-#output = linear_classifier(output)
-#print(output)
+
 
 
 x = torch.ones((1, 3, 224, 224))
 out = model(x)
-print("out: ", out)
+print("model out", out)
